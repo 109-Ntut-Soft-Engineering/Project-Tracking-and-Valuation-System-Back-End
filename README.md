f71ff42550715d35243fafb26a842a67abf67a2a
gougon <freddy2531@gmail.com>
2020-11-20 19:13:49 +0800

    [Feature] Initialize database

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 安裝套件 : pip install -r requirements.txt
+
+Restful Flask教學 : https://www.youtube.com/watch?v=GMppyAPbLYk&t=3043s&ab_channel=TechWithTim
+
+Cloud firestore教學 : https://firebase.google.com/docs/firestore/quickstart?hl=zh-cn#python
--- src/app.py
+++ src/app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_restful import Api
 from flask_cors import CORS
+import firebase_admin
+from firebase_admin import credentials, firestore
 import config
 from resources.example_controller import ExampleController
 
@@ -8,6 +10,10 @@ app = Flask("soft engineering")
 app.config.from_object(config)
 api = Api(app)
 CORS(app, origin='*')
+
+cred = credentials.Certificate('firebaseKey.json')
+firebase_admin.initialize_app(cred)
+db = firestore.client()
 
 api.add_resource(ExampleController, '/example/')
 
