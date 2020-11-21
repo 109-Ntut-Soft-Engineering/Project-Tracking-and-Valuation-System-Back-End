3ff072cd252fbc974d2e3d315333402d84514638
wei <wei.141227@gmail.com>
2020-11-21 22:54:01 +0800

    database class 新增 uid

--- src/db.py
+++ src/db.py
@@ -1,10 +1,11 @@
 import firebase_admin
-from firebase_admin import credentials, firestore
+from firebase_admin import credentials, firestore, auth
 import config as config
 import sys
 
+
 class Database():
-    def __init__(self):
+    def __init__(self, idToken):
         firebaseKey = './software-engineer-back-end/src/'
         if not config.TEST:
             firebaseKey += 'firebaseKey.json'
@@ -13,6 +14,8 @@ class Database():
         cred = credentials.Certificate(firebaseKey)
         firebase_admin.initialize_app(cred)
         self._db = firestore.client()
+        decoded_token = auth.verify_id_token(idToken)
+        self.uid = decoded_token['uid']
 
     @property
     def db(self):
