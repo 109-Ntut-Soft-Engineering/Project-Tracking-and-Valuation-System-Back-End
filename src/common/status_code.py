643008a21e4d2662c0477b08df5cac25edb69570
gougon <freddy2531@gmail.com>
2020-11-28 12:53:09 +0800

    [Feature] Update project complete

--- src/common/status_code.py
+++ src/common/status_code.py
@@ -1,3 +1,8 @@
 OK = 200
 BAD_REQUEST = 400
 NOT_FOUND = 404
+
+client_error_code = [OK, BAD_REQUEST, NOT_FOUND]
+
+def is_client_error(status_code):
+    return status_code in client_error_code
--- src/resources/project_resource.py
+++ src/resources/project_resource.py
@@ -3,6 +3,7 @@ from flask_restful import Resource, reqparse
 from resources.base_resource import BaseResource
 from models.project import Project
 from common import status_code
+from common.status_code import is_client_error
 from common.util import is_iter_empty
 
 class ProjectResource(BaseResource):
@@ -11,8 +12,8 @@ class ProjectResource(BaseResource):
 
     def get(self, name=None):
         data = self.__project_information(name)
-        if data == status_code.NOT_FOUND:
-            abort(status_code.NOT_FOUND)
+        if is_client_error(data):
+            abort(data)
         return jsonify(data)
 
     def post(self):
@@ -22,8 +23,8 @@ class ProjectResource(BaseResource):
         args = parser.parse_args()
 
         message = self.__add_project(args['name'], args['owner'])
-        if message == status_code.BAD_REQUEST:
-            abort(status_code.BAD_REQUEST)
+        if is_client_error(message):
+            abort(message)
         return message
 
     def delete(self):
@@ -32,8 +33,20 @@ class ProjectResource(BaseResource):
         args = parser.parse_args()
 
         message = self.__delete_project(args['name'])
-        if message == status_code.NOT_FOUND:
-            abort(status_code.NOT_FOUND)
+        if is_client_error(message):
+            abort(message)
+        return message
+
+    def patch(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('name', required=True, help='name is required.')
+        parser.add_argument('owner', required=False)
+        parser.add_argument('repositories', required=False)
+        args = parser.parse_args()
+
+        message = self.__update_project(args['name'], args['owner'], args['repositories'])
+        if is_client_error(message):
+            return message
         return message
 
     def __project_information(self, name=None):
@@ -72,9 +85,31 @@ class ProjectResource(BaseResource):
     def __delete_project(self, name):
         if not self.__is_project_name_exist(name):
             return status_code.NOT_FOUND
+
         projects = self.db.collection(u'projects').where(u'name', u'==', name).stream()
         for project in projects:
             project.reference.delete()
         return status_code.OK
 
-    
+    def __update_project(self, name, owner, repositories):
+        if not self.__is_project_name_exist(name):
+            return status_code.NOT_FOUND
+
+        project = self.__get_unique(self.db.collection(u'projects').where(u'name', u'==', name))
+        project_dict = project.to_dict()
+
+        update_data = {}
+        if owner != None:
+            update_data[u'owner'] = owner
+        elif set(owner) & set(project_dict['owner']):
+            return status_code.BAD_REQUEST
+        if repositories != None:
+            update_data[u'repositories'] = repositories
+        elif set(repositories) & set(project_dict['repositories']):
+            return status_code.BAD_REQUEST
+        
+        project.set(update_data, merge=True)
+        return status_code.OK
+
+    def __get_unique(self, collection):
+        return next(collection.stream())
