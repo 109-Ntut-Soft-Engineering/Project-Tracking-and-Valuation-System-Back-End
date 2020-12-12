b50d65eaff476d0c508ef2db975b5c51f1f9996c
s88037zz <s88037zz@gmail.com>
2020-12-12 19:49:43 +0800

    [feat] add gitlab api requester

---
+++ src/utilities/gitlab_api_requester.py
@@ -0,0 +1,62 @@
+import gitlab
+
+
+class GitlabApiRequester:
+    def __init__(self, token):
+        self.gitlab = gitlab.Gitlab('https://gitlab.com/', private_token=token)
+        self.gitlab.auth()
+
+    def get_project_by_name(self, group_name, project_name):
+        group = self.gitlab.groups.get(group_name)
+        if group is None:
+            print('Cant find', group_name)
+            return None
+        else:
+            print('Find ', group.name)
+            projects = group.projects.list(searc=project_name, all=True)
+            print(' there are {} projects'.format(len(projects)))
+            for project in projects:
+                if project.path_with_namespace == group_name+'/'+project_name:
+                    return project
+            return None
+
+    def get_project_by_url(self, url):
+        'https://gitlab.com/s88037zz/soft-engineering-front-end.git'
+        if 'https://gitlab.com/' in url:
+            url = url.replace('https://gitlab.com/', '').replace('.git', '')
+            group_name, project_name = url.split('/')
+            print('gp_name, pro_name:', group_name, project_name)
+            return self.get_project_by_name(group_name, project_name)
+        else:
+            raise ValueError('Url is not from gitlab source.')
+
+
+    def get_commits(self, project):
+        return self.gitlab.projects.get(project.id, lazy=True).commits.list()
+
+    def get_issues(self, project):
+        return self.gitlab.projects.get(project.id, lazy=True).issues.list()
+
+    def get_code_freq(self):
+        return
+
+if __name__ == '__main__':
+    # gl = gitlab.Gitlab('https://gitlab.com/', private_token='eijr9MQx7NCzwSnGfBFK')
+    # gl.auth()
+
+    token = 'eijr9MQx7NCzwSnGfBFK'
+    gitlab = GitlabApiRequester(token)
+    url = 'https://gitlab.com/ase/ase.git'
+
+    # search by url
+    project = gitlab.get_project_by_url(url)
+    print(type(project))
+    print(project.id)
+
+    commits = gitlab.get_commits(project)
+    for index, commit in enumerate(commits):
+        print('Idx: {}, commit:{}'.format(index, commit.title))
+
+    issues = gitlab.get_issues(project)
+    for index, issue in enumerate(issues):
+        print('Idx: {}, issue:{}'.format(index, issue.title))
