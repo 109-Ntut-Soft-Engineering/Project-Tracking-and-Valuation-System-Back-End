93315183a74da0ffd0069919ce75d02af9f34116
gougon <freddy2531@gmail.com>
2020-12-28 00:11:07 +0800

    [Test] test project

--- src/entities/project.py
+++ src/entities/project.py
@@ -9,47 +9,28 @@ class Project():
     @staticmethod
     def from_dict(source):
         project = Project(
-            source[u'name'], source[u'owner'], source[u'collaborator'], source[u'repositories'], source[u'updated'])
-
-        # if u'name' in source:
-        #     project.name = source[u'name']
-        # if u'owner' in source:
-        #     project.owner = source[u'owner']
-        # if u'repositories' in source:
-        #     project.repositories = source[u'repositories']
+            source[u'name'], 
+            source[u'owner'], 
+            source[u'collaborator'], 
+            source[u'repositories'], 
+            source[u'updated']
+        )
 
         return project
 
     def to_dict(self):
 
-        dest = {
-            'name': self.name,
-            'owner': self.owner,
-            # 'collaborator': self.collaborator,
-            # 'repositories': self.repositories,
-            'updated': self.updated.__str__()
-        }
+        dest = {}
 
-        # if self.pid:
-        #     dest['pid'] = self.pid
-        # if self.name:
-        #     dest['name'] = self.name
-        # if self.owner:
-        #     dest['owner'] = self.owner
-        # if self.repositories:
-        #     dest['repositories'] = self.repositories
-        # if self.updated:
-        #     dest['updated'] = self.updated.__str__()
+        if self.name:
+            dest['name'] = self.name
+        if self.owner:
+            dest['owner'] = self.owner
+        if self.owner:
+            dest['collaborator'] = self.collaborator
+        if self.repositories:
+            dest['repositories'] = self.repositories
+        if self.updated:
+            dest['updated'] = self.updated.__str__()
 
         return dest
-
-    def __repr__(self):
-        return (
-            f'project(\
-                name={self.name}, \
-                owner={self.owner}, \
-                collaborator={self.collaborator}, \
-                repositories={self.repositories},\
-                updated={self.updated.__str__()}\
-            )'
-        )
