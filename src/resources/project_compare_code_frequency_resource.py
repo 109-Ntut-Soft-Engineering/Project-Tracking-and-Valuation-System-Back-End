8c11be66c01dc4d93272684226cf7da162668beb
s88037zz <s88037zz@gmail.com>
2021-01-03 08:56:48 +0800

    Fix - Code Frequency model error

--- src/resources/project_compare_code_frequency_resource.py
+++ src/resources/project_compare_code_frequency_resource.py
@@ -1,11 +1,12 @@
 from models.project_code_frequency_model import ProjectCodeFrequencyModel
 from flask_restful import Resource
 from common import status_code
+from src.conn_tool import ConnTool
 
 
 class ProjectCompareCodeFrequencyResource(Resource):
     def __init__(self):
-        self._model = ProjectCodeFrequencyModel()
+        self._model = ProjectCodeFrequencyModel(ConnTool)
 
     def get(self, pid1, pid2):
         # try:
